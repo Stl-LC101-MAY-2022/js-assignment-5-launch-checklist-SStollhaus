6bf152ff7f55bf5a9d352fdb0bebfd8d0ea71381
Sean Stollhaus <seanstollhaus@Seans-MBP.local>
2022-08-02 21:04:29 -0500

    8/2/22

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { addDestinationInfo } from "./scriptHelper";
+
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
 
@@ -10,13 +12,16 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   async function myFetch() {
+  let selectedPlanet =
+ pickPlanet(listedPlanets);  
+ addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.imageUrl);
+    })
+//    async function myFetch() {
     
-    let planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response){
-        return response.json();
-});
-    return planetsReturned;
-}
+//     let planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response){
+//         return response.json();
+// });
+//     return planetsReturned;
+// }
 
 });
